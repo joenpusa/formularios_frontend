3a868c92fe21e33d5d89e98d716ca29cfea010a9
joenpusa <joenpusa@gmail.com>
2025-02-14 21:11:13 -0500

    cambio en sincronizador de vue

--- vue.config.js
+++ vue.config.js
@@ -13,3 +13,21 @@ module.exports = defineConfig({
     ],
   },
 });
+
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    name: ''
+  },
+  mounted() {
+    if (localStorage.name) {
+      this.name = localStorage.name;
+    }
+  },
+  watch: {
+    name(newName) {
+      localStorage.name = newName;
+    }
+  }
+});
